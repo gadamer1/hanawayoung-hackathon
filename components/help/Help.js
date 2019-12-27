2cee00d91a8c518a59e3ee732ae68e3f4be0e60d
gadamer1 <dngmlws21@korea.ac.kr>
2019-12-28 05:16:19 +0900

    a

--- components/help/Help.js
+++ components/help/Help.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { View } from "react-native";
-import { Button } from "react-native-elements";
+import { Button, Input } from "react-native-elements";
 import axios from "axios";
 const Help = props => {
+  const [detail, setDetail] = useState("1s");
+  const { location } = useSelector(state => state.user);
   const _onPressButton = () => {
-    return axios.get("http://54.180.101.221:4000/help");
+    if (detail.trim() === "") {
+      return alert("정확하게 설명해주세요");
+    }
+
+    const data = {
+      x_wgs84: location.longitude,
+      y_wgs84: location.latitude,
+      detail
+    };
+    return axios.post("http://54.180.101.221:4000/help", data);
   };
 
   return (
-    <View>
+    <View style={{ flex: 1 }}>
+      <Input
+        style={{ height: 300, borderColor: "gray", borderWidth: 1 }}
+        value={detail}
+        onChangeText={text => setDetail(text)}
+      />
       <Button title="전송" onPress={_onPressButton} />
     </View>
   );
